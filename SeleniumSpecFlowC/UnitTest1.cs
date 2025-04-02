eccf97dabb3a7afbd3088f737a4dcaee60ba4cbb
kevinsmba <kevinsmba@gmail.com>
2025-04-02 12:07:53 -0500

    first commit

---
+++ SeleniumSpecFlowC/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace SeleniumSpecFlowC
+{
+    public class Tests
+        //git adding verification in comments 1
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
